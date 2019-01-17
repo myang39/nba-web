43b745412224a23bccde4306f82aad20e8227aa7
Minkang Yang <minkangyang@gmail.com>
2019-01-16 20:56:50 -0500

    main skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import {TopBar} from "./TopBar";
+import {Main} from "./Main";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TopBar/>
+        <TopBar />
+        <Main />
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Main extends React.Component {
+    render () {
+        return (
+            <div className="main">
+                <div>Profile</div>
+                <div>shotChart</div>
+            </div>
+        );
+    }
+}
+
