d9a8c8677d1e4c63bfa7e695d59cb2d2ab990cb7
Minkang Yang <minkangyang@gmail.com>
2019-01-16 21:37:12 -0500

    get player info from api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import nba from 'nba';
+import {Profile} from "./Profile";
 
 export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {}
+    }
+
+    // nba.stats.playerInfo
+    componentDidMount() {
+        nba.stats.playerInfo({PlayerID: this.state.playerId})
+            .then((response) => {
+                console.log("PlayerInfo:", response);
+                let playerInfo = Object.assign(
+                    {}, response.commonPlayerInfo[0], response.playerHeadlineStats[0]
+                );
+                this.setState({
+                   playerInfo
+                });
+            })
+            .catch(console.log);
+    }
+
     render () {
         return (
             <div className="main">
-                <div>Profile</div>
+                <Profile playerInfo={this.state.playerInfo}/>
                 <div>shotChart</div>
             </div>
         );
