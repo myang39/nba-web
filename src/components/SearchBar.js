4389e0eaf5256acdd4a3c3718a465b1b51712a3f
Minkang Yang <minkangyang@gmail.com>
2019-01-19 17:59:22 -0500

    complete search bar styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Icon, Input, AutoComplete } from 'antd';
 import nba from 'nba';
+import {PROFILE_PIC_URL_PREFIX} from "../constants";
+
+const Option = AutoComplete.Option;
 
 export class SearchBar extends React.Component {
     state = {
@@ -14,7 +17,16 @@ export class SearchBar extends React.Component {
     handleSearch = (value) => {
         this.setState({
             dataSource: !value ? [] : nba.searchPlayers(value).map(
-                ({fullName}) => fullName
+                ({fullName, playerId}) => (
+                    <Option key={fullName}>
+                        <img
+                            className="player-option-image"
+                            src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
+                            alt="Profile"
+                        />
+                        <span className="player-option-label">{`${fullName}`}</span>
+                    </Option>
+                )
             )
         });
     }
